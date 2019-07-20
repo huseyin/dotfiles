59af56b55aacaeb3b1190a275a8ce21f4482104b
Hüseyin Tekinaslan <htkaslan@gmail.com>
2019-07-20 23:20:06 +0300

    Add a bootstrap for the provisioning

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,1 +1,12 @@
 #!/usr/bin/env bash
+
+# shellcheck disable=1090
+source <(curl -fsSL https://she.alaturka.io/source) -boot
+
+enter ./catalog
+	enter ./base
+		try font
+		try staff
+		try timezone
+	leave
+leave
